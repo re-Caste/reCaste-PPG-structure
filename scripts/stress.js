64382e754721bb06ae84da0d446e137e6bfb8e50
reCaste <93339312+re-Caste@users.noreply.github.com>
2025-10-27 13:14:44 +0000

    Update stress.js
    Bugfix to handle 0 heatcap characters

--- scripts/stress.js
+++ scripts/stress.js
@@ -12,9 +12,15 @@ export async function overheatRework(state) {
         const excess = actor.system.heat.value - actor.system.heat.max; // Get overflowed heat
         console.log("Excess heat: "+excess);
 
-        await actor.toggleStatusEffect("exposed", {active:true});
-        await actor.toggleStatusEffect("overheated", {active:true});
-        console.log("Statuses applied");
+        if (actor.system.heat.max !== 0) {
+            await actor.toggleStatusEffect("exposed", {active:true});
+            try {
+                await actor.toggleStatusEffect("overheated", {active:true});
+            } catch {}; // Handles case if this is used without overheated being defined (e.g. running One Stress for All without the PPG lcp)
+            console.log("Statuses applied");
+        } else {
+            console.log("No heat cap, skipping exposed/overheated")
+        };
 
         const dmgConfig = {
             hit_results: [{
