8b24a2b71e4eb8692bc5cc4f419a7e8f0f0d99ef
reCaste <93339312+re-Caste@users.noreply.github.com>
2026-01-22 18:13:07 +0000

    v0.4
    Added toggles in settings for each part of the Reworks
    
    Added NPC Recharge Heat as detailed in PPG Appendix

--- scripts/module.js
+++ scripts/module.js
@@ -1,34 +1,59 @@
 import { moduleId } from "./consts.js";
-import { handleHeat } from "./damage.js";
+import { bindSettings, setting_id_struct, setting_id_stress, setting_id_recharge_heat  } from "./settings.js";
+import { handleHeat, rechargeHeat } from "./damage.js";
 import { overheatRework } from "./stress.js";
 import { structureRework, structureInsertTraumaRework } from "./structure.js";
 
+Hooks.once("init", () => {
+    bindSettings();
+})
+
 // Flows //
 Hooks.once("lancer.registerFlows", (flowSteps, flows) => {
+    // Settings are registered AFTER lancer initialises, so we have to look at the world's database for these values
+    const enableStruct = game.settings.storage.get("world").getSetting(`${moduleId}.${setting_id_struct}`)?.value ?? true;
+    const enableStress = game.settings.storage.get("world").getSetting(`${moduleId}.${setting_id_stress}`)?.value ?? true;
+    const enableRchgHeat = game.settings.storage.get("world").getSetting(`${moduleId}.${setting_id_recharge_heat}`)?.value ?? true;
+
     //Structure - Is this messy as all hell? Yeah. Does it work? Yeah.
-    const strucFlow = flows.get("StructureFlow");
-    flowSteps.set(`${moduleId}:structureRework`, structureRework);
-    strucFlow?.insertStepBefore("rollStructureTable", `${moduleId}:structureRework`);
-    strucFlow?.insertStepBefore("rollStructureTable", "noStructureRemaining");
-    flowSteps.set(`${moduleId}:insertTraumaButton`, structureInsertTraumaRework)
-    strucFlow?.insertStepBefore("rollStructureTable", `${moduleId}:insertTraumaButton`);
-    strucFlow?.insertStepBefore("rollStructureTable", "structureInsertCascadeRollButton");
-    strucFlow?.insertStepBefore("rollStructureTable", "printStructureCard");
-    strucFlow?.insertStepBefore("rollStructureTable", "secondaryStructureRoll");
-    strucFlow?.insertStepBefore("rollStructureTable", "printSecondaryStructureCard");
-    flowSteps.set("stopFlow", stopFlow);
-    strucFlow?.insertStepBefore("rollStructureTable", "stopFlow");
+    if (enableStruct === true) {
+        const strucFlow = flows.get("StructureFlow");
+        flowSteps.set(`${moduleId}:structureRework`, structureRework);
+        strucFlow?.insertStepBefore("rollStructureTable", `${moduleId}:structureRework`);
+        strucFlow?.insertStepBefore("rollStructureTable", "noStructureRemaining");
+        flowSteps.set(`${moduleId}:insertTraumaButton`, structureInsertTraumaRework)
+        strucFlow?.insertStepBefore("rollStructureTable", `${moduleId}:insertTraumaButton`);
+        strucFlow?.insertStepBefore("rollStructureTable", "structureInsertCascadeRollButton");
+        strucFlow?.insertStepBefore("rollStructureTable", "printStructureCard");
+        strucFlow?.insertStepBefore("rollStructureTable", "secondaryStructureRoll");
+        strucFlow?.insertStepBefore("rollStructureTable", "printSecondaryStructureCard");
+        flowSteps.set("stopFlow", stopFlow);
+        strucFlow?.insertStepBefore("rollStructureTable", "stopFlow");
+    };
 
     //Stress
-    const stressFlow = flows.get("OverheatFlow");
-    flowSteps.set(`${moduleId}:overheatRework`, overheatRework)
-    stressFlow?.insertStepBefore("preOverheatRollChecks", `${moduleId}:overheatRework`);
-    stressFlow?.insertStepBefore("preOverheatRollChecks", "stopFlow");
+    if (enableStress === true) {
+        const stressFlow = flows.get("OverheatFlow");
+        flowSteps.set(`${moduleId}:overheatRework`, overheatRework)
+        stressFlow?.insertStepBefore("preOverheatRollChecks", `${moduleId}:overheatRework`);
+        stressFlow?.insertStepBefore("preOverheatRollChecks", "stopFlow");
+    };
+
+    //Recharge Heat
+    if (enableRchgHeat === true) {
+        const activFlow = flows.get("ActivationFlow");
+        const sysFlow = flows.get("SystemFlow");
+        const techFlow = flows.get("TechAttackFlow");
+        flowSteps.set(`${moduleId}:rechargeHeat`, rechargeHeat);
+        activFlow?.insertStepBefore("updateItemAfterAction", `${moduleId}:rechargeHeat`);
+        sysFlow?.insertStepBefore("updateItemAfterAction", `${moduleId}:rechargeHeat`);
+        techFlow?.insertStepBefore("updateItemAfterAction", `${moduleId}:rechargeHeat`);
+    }
 
     //Damage
-    const damageFlow = flows.get("DamageRollFlow")
-    flowSteps.set(`${moduleId}:handleHeat`, handleHeat)
-    damageFlow?.insertStepBefore("printDamageCard", `${moduleId}:handleHeat`)
+    const damageFlow = flows.get("DamageRollFlow");
+    flowSteps.set(`${moduleId}:handleHeat`, handleHeat);
+    damageFlow?.insertStepBefore("printDamageCard", `${moduleId}:handleHeat`);
 })
 
 // Helpers //
